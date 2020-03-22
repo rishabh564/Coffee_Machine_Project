162747f90a6ad313146b89c2839d007c2d25fddc
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:33:54 +0530

    some things are added

--- Problems/The sum of digits/src/Main.java
+++ Problems/The sum of digits/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // put your code here
         int a = scanner.nextInt();
         int r = 0;
         int sum = 0;
