d71072135bce18c989b7d3f92699c9fd6858dd40
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:28:49 +0530

    sum variable is initialed

--- Problems/The sum of digits/src/Main.java
+++ Problems/The sum of digits/src/Main.java
@@ -5,5 +5,11 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         // put your code here
         int a = scanner.nextInt();
+        int temp = a;
+        int r = 0;
+        int sum = 0;
+        while (a > 0) {
+            r = a
+        }
     }
 }
