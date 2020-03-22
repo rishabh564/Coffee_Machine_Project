aab8a44f13b71556d3fd6ae48691754873d82025
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:57:04 +0530

    int variable is introduced

---
+++ Problems/Next even number/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int a = scanner.nextInt();
+    }
+}
