94d844ce52f4ea7a070b731673a7adcaecb351bf
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:25:22 +0530

    print the first digit of a no

--- Problems/First digit/src/Main.java
+++ Problems/First digit/src/Main.java
@@ -4,6 +4,6 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-
+        System.out.println(scanner.nextInt() / 10);
     }
 }
