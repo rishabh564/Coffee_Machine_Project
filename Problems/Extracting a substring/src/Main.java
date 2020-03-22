2ed61858d99732a5580a2302bd42739f384332c1
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-23 01:03:31 +0530

    string variable is used to get value

---
+++ Problems/Extracting a substring/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int a = sc.nextInt();
+
+
+    }
+}
