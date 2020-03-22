228181514bb3b3812a35987c6d92c0d40f582514
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:33:15 +0530

    some errors are removed

--- Problems/The sum of digits/src/Main.java
+++ Problems/The sum of digits/src/Main.java
@@ -8,10 +8,10 @@ class Main {
         int r = 0;
         int sum = 0;
         while (a > 0) {
-            r = a & 10;
+            r = a % 10;
             sum += r;
             a = a / 10;
         }
-
+        System.out.println(sum);
     }
 }
