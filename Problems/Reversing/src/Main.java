7b619e3cf1c0fd1f3864c9782961da4963151b53
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:39:55 +0530

    reverse variable is used for reverse no

--- Problems/Reversing/src/Main.java
+++ Problems/Reversing/src/Main.java
@@ -6,9 +6,11 @@ class Main {
         // put your code here
         int a = scanner.nextInt();
         int temp = 0;
+        int reverse = 0;
         while (a > 0) {
             temp = a % 10;
-
+            reverse = reverse * 10 + temp;
+            a = a / 10;
         }
     }
 }
