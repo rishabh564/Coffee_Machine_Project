f4eaf3bea8725221ec2cdbaa2dd99f2e1b55a889
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-03-22 23:48:50 +0530

    difference is calculated

--- Problems/Difference of times/src/Main.java
+++ Problems/Difference of times/src/Main.java
@@ -7,5 +7,11 @@ class Main {
         int h1 = scanner.nextInt();
         int m1 = scanner.nextInt();
         int s1 = scanner.nextInt();
+        int h2 = scanner.nextInt();
+        int m2 = scanner.nextInt();
+        int s2 = scanner.nextInt();
+        int sec1 = (h2 - h1) * 3600;
+
+
     }
 }
