3add513ff7e057b92f4cbbe18fab5b7dcecdf5ab
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-01 23:08:11 +0530

    taking user input: about how many cups of coffee, user will need

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,14 +1,13 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+
     }
 }
