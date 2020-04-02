b7708189787642e6dc9803f8c651b19ab8749b07
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-02 20:53:40 +0530

    if b is false then range is 10 to 20

--- Problems (6)/Groundhogs at a party/src/Main.java
+++ Problems (6)/Groundhogs at a party/src/Main.java
@@ -5,6 +5,12 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         boolean b = sc.nextBoolean();
+        if (b == false) {
+            if (a >= 10 && a <= 20)
+                System.out.println(true);
+            else
+                System.out.println(false);
+        }
 
     }
 }
