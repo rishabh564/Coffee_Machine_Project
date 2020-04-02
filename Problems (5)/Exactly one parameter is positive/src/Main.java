6542b89029fdd93b2ed42f7e8714f62ba1fa8025
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-02 20:41:26 +0530

    operation done for c variable

--- Problems (5)/Exactly one parameter is positive/src/Main.java
+++ Problems (5)/Exactly one parameter is positive/src/Main.java
@@ -25,7 +25,13 @@ class Main {
             System.out.println(true);
         else if (b > 0 && (c < 0 && a < 0))
             System.out.println(true);
-
-
+        else if (c > 0 && (a == 0 && b == 0))
+            System.out.println(true);
+        else if (c > 0 && (a < 0 && b == 0))
+            System.out.println(true);
+        else if (c > 0 && (b < 0 && a == 0))
+            System.out.println(true);
+        else if (c > 0 && (b < 0 && a < 0))
+            System.out.println(true);
     }
 }
