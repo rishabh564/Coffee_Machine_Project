66ab9ccb371014c7b90ea6603ec558665c31c93b
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-02 20:04:51 +0530

    three variables are used for user input

---
+++ Problems (3)/Is between/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+    }
+}
