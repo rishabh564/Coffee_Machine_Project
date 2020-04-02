065351b27520d186e0be3b78812d37947d24468a
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-02 20:17:26 +0530

    variable is used to get input

--- Problems (4)/Check the value/src/Main.java
+++ Problems (4)/Check the value/src/Main.java
@@ -4,6 +4,9 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        Scanner sc
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
     }
 }
