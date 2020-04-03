15fff21f1172a5f247a47f3e93eddbb42b00db40
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 20:40:41 +0530

    program to find the given year is a leap year

--- Problems (13)/Leap year/src/Main.java
+++ Problems (13)/Leap year/src/Main.java
@@ -4,10 +4,15 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if (a % 100 != 0) {
-            if (a % 4 == 0)
+        if (a % 4 == 0) {
+            if (a % 100 == 0) {
+                if (a % 400 == 0)
+                    System.out.println("Leap");
+                else
+                    System.out.println("Regular");
+            } else
                 System.out.println("Leap");
-        }
-
+        } else
+            System.out.println("Regular");
     }
 }
