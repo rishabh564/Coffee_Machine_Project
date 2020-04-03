5bf24b2649021fea57897affa1d114071446f90e
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 18:11:21 +0530

    while loop is used t0 get reverse no

---
+++ Problems (12)/Symmetrical number/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = 0;
+        int r = 0;
+        int k = a;
+        while (a > 0) {
+            n = a % 10;
+            r = (r * 10) + n;
+            a = a / 10;
+        }
+
+    }
+}
