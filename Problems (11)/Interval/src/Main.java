5e9dc763354581da809d226ebadf944484888ef3
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 18:04:05 +0530

    if conditions are false then print "False"

--- Problems (11)/Interval/src/Main.java
+++ Problems (11)/Interval/src/Main.java
@@ -6,6 +6,7 @@ class Main {
         int a = sc.nextInt();
         if ((a > -15 && a <= 12) || (a > 14 && a < 17) || (a >= 19 && a < Integer.MAX_VALUE))
             System.out.println("True");
-
+        else
+            System.out.println("False");
     }
 }
