fc1e6e1d85151def2ed09dac7213eab5af753d30
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 18:02:56 +0530

    if conditions are true then print "True"

---
+++ Problems (11)/Interval/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if ((a > -15 && a <= 12) || (a > 14 && a < 17) || (a >= 19 && a < Integer.MAX_VALUE))
+            System.out.println("True");
+
+    }
+}
