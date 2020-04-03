48fb536c4bffe8dd0c455eb8babd10c8f1ffabb1
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-04-03 18:27:25 +0530

    if condition is true then it is leap year

---
+++ Problems (13)/Leap year/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 100 != 0) {
+            if (a % 4 == 0)
+                System.out.println("Leap");
+        }
+
+    }
+}
